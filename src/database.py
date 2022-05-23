ef70842e4d7b0e6d3df941bc36ad804b6c9fbc42
Sierra Heins <sierra.heins@evergy.com>
2022-05-23 14:50:49 -0500

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,19 +1,24 @@
 import csv
+from src.deadlined_reminders import DeadlinedReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
-
-    with f:
-        reader = csv.reader(f)
-
-        for row in reader:
-            print()
-            for e in row:
+@@ -12,10 +13,13 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text):
+def add_reminder(reminder):
+    print()
+    reminder = input("What would you like to be reminded about?: ")
+
+def add_reminder(text, date, ReminderClass):
+    reminder = ReminderClass(text, date)
+
+    if not isinstance(reminder, DeadlinedReminder):
+        raise TypeError('Invalid Reminder Class')
+
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder])
+        writer.writerow(reminder)
